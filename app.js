58fa3052b68c144fdb056edf170cf5e20eea3b87
Tipene Hughes <tipenehughes@gmail.com>
2020-10-02 12:47:12 -0400

    Updated image carousel sizing and added navbar scroll functionality

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 const navigation = document.getElementById("navigation");
 const navLinks = document.querySelectorAll(".nav-link");
 const navImage = document.querySelector(".nav-image");
+let elem = document.querySelector(".main-carousel");
 
 navigation.addEventListener("mouseenter", () => {
     navLinks.forEach((item) => {
         item.classList.add("nav-link-active");
     });
+    navigation.classList.add("navigation-hover");
     navImage.classList.add("nav-image-active");
 });
 
@@ -15,12 +17,64 @@ navigation.addEventListener("mouseleave", () => {
     navLinks.forEach((item) => {
         item.classList.remove("nav-link-active");
     });
+    navigation.classList.remove("navigation-hover");
     navImage.classList.remove("nav-image-active");
+    if (window.scrollY > 10) {
+        navigation.classList.add("scrolled-down");
+        navigation.classList.remove("scrolled-up");
+    }
+});
+
+// Nav bar scroll
+
+function upAction() {
+    navigation.classList.remove("scrolled-down");
+    navigation.classList.add("scrolled-up", "navigation-hover");
+    navImage.classList.add("nav-image-active");
+    navLinks.forEach((item) => {
+        item.classList.add("nav-link-active");
+    });
+}
+
+function downAction() {
+    navigation.classList.add("scrolled-down");
+    navigation.classList.remove("scrolled-up", "navigation-hover");
+    navImage.classList.remove("nav-image-active");
+    navLinks.forEach((item) => {
+        item.classList.remove("nav-link-active");
+    });
+}
+
+const scrollTop = function () {
+    return window.scrollY;
+};
+
+let scrollState = 0;
+
+const scrollDetect = function (down, up) {
+    // Current scroll position
+    const currentScroll = scrollTop();
+    if (currentScroll > scrollState) {
+        down();
+    } else if (window.scrollY === 0) {
+        navLinks.forEach((item) => {
+            item.classList.remove("nav-link-active");
+        });
+        navigation.classList.remove("navigation-hover");
+        navImage.classList.remove("nav-image-active");
+    } else {
+        up();
+    }
+    // Set previous scroll position
+    scrollState = scrollTop();
+};
+
+window.addEventListener("scroll", function () {
+    scrollDetect(downAction, upAction);
 });
 
 // Flickity
 
-let elem = document.querySelector(".main-carousel");
 let flkty = new Flickity(elem, {
     // options
     cellAlign: "center",
