f5e9f329bcfe3a118dd3a5d85f0531d8b069a47a
Tipene Hughes <tipenehughes@gmail.com>
2020-10-01 21:03:18 -0400

    Added new section and image carousel

--- app.js
+++ app.js
@@ -2,15 +2,32 @@
 
 const navigation = document.getElementById("navigation");
 const navLinks = document.querySelectorAll(".nav-link");
+const navImage = document.querySelector(".nav-image");
 
 navigation.addEventListener("mouseenter", () => {
     navLinks.forEach((item) => {
-        item.classList.add("nav-link-hover");
+        item.classList.add("nav-link-active");
     });
+    navImage.classList.add("nav-image-active");
 });
 
 navigation.addEventListener("mouseleave", () => {
     navLinks.forEach((item) => {
-        item.classList.remove("nav-link-hover");
+        item.classList.remove("nav-link-active");
     });
+    navImage.classList.remove("nav-image-active");
+});
+
+// Flickity
+
+let elem = document.querySelector(".main-carousel");
+let flkty = new Flickity(elem, {
+    // options
+    cellAlign: "center",
+    contain: true,
+    imagesLoaded: true,
+    wrapAround: true,
+    isLoaded: true,
+    autoPlay: 4000,
+    fullscreen: true,
 });
